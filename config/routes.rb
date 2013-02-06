c5b2db2243a40bb20477a923944a895420cceef7
Ivan Bisevac <ivan.bisevac@gmail.com>
2013-02-06 23:57:54 +0100

    Finish layout and routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Honeypot::Application.routes.draw do
   get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   authenticated :user do
     root :to => 'home#index'
   end
-  root :to => "home#index"
+  root :to => "static_pages#home"
   devise_for :users
   resources :users
 end
