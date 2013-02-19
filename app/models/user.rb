300c49d3ed12e53abfd8d589a85d733efaf633a5
Ivan Bisevac <ivan.bisevac@gmail.com>
2013-02-19 22:20:33 +0100

    Added Last name to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :first_name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
   
-  validates_presence_of :first_name
-  validates_uniqueness_of :first_name, :email, :case_sensitive => false
+  validates_presence_of :first_name, :last_name
+  validates_uniqueness_of :email, :case_sensitive => false
 end
