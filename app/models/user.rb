b775584f4d88562ba4d2d184bc1b5803398fcb91
Ivan Bisevac <ivan.bisevac@gmail.com>
2013-03-15 11:16:50 +0100

    Skip confirmation for facebook. Get first name and last name instead of name attribute from facebook.

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,13 +17,13 @@ class User < ActiveRecord::Base
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
-      user = User.create(first_name:auth.extra.raw_info.name,
-                          last_name:auth.extra.raw_info.name,
+      user = User.new(first_name:auth.extra.raw_info.first_name,
+                          last_name:auth.extra.raw_info.last_name,
                           provider:auth.provider,
                           uid:auth.uid,
                           email:auth.info.email,
                           password:Devise.friendly_token[0,20]
-                          )
+                          ).skip_confirmation!.save!
     end
     user
   end
