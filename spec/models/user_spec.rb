4b1328d4d0d6bd56b767e683d7679cc3a95ce0ad
Иван Бишевац <ivan.bisevac@gmail.com>
2013-10-13 00:16:36 +0200

    Refactoring. Deleted unnecessary specs and modified some files.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ describe User do
 		@user = FactoryGirl.create(:user)
 	end
 
-  context "fullname" do
+  describe "#fullname" do
   	it "should return fullname if there is first name and last name" do
   		@user.fullname.should eq 'Foo Bar'
   	end
